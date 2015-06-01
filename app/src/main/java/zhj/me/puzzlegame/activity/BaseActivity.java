8b950951898d3fe75dd7c6de2076eddbf4d7ab3d
Gavin <wssgzhj@163.com>
2015-06-01 09:48:19 +0800

    change some activity class

---
+++ app/src/main/java/zhj/me/puzzlegame/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package zhj.me.puzzlegame.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Gavin on 2015-05-29.
+ */
+public class BaseActivity extends FragmentActivity{
+}
